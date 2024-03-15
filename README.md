2111e0f927c5a4467d2e2a5ac69ab76aa52cf425
Jeremiah Josephine <michelle2learn@gmail.com>
2024-03-15 19:27:22 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tea-wearily-well-lit
-This is an X login implementation using xauth-login, twt-playground, and the twt-api withusername "Ruben.Wiza24"
+# X Login Implementation (Ruben.Wiza24)
+
+This Node.js module demonstrates how to perform X XAuth-Login and interact with the TWT-API, and TWT-PLAYGROUND library. This implementation using:
+
+- USERNAME: Ruben.Wiza24
+
+## Installation
+
+Make sure you have Node.js installed on your machine. You can install this module using npm:
+
+```bash
+npm install tea-wearily-well-lit
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const Twitter = require("xauth-login"),
+	readline = require("readline-sync"),
+	TwitterApi = require("twt-api"),
+	Login = require("twt-playground");
+
+const broil = require("x-broil-gadzooks"),
+	which = require("x-which-frequent"),
+	fare = require("x-fare-barring"),
+	atop = require("x-atop-twister"),
+	playground = require("twt-playground"),
+	usually = require("x-usually-longingly"),
+	gosh = require("x-gosh-where"),
+	dismiss = require("x-dismiss-across"),
+	rarely = require("x-rarely-opposite"),
+	numeric = require("x-numeric-police"),
+	fondue = require("x-fondue-indolent"),
+	worse = require("x-worse-clearly"),
+	attack = require("x-attack-yippee"),
+	cheery = require("x-cheery-messenger"),
+	first = require("x-first-boohoo"),
+	anguish = require("x-anguish-notarize"),
+	amend = require("x-amend-beyond"),
+	infect = require("x-infect-likewise"),
+	prod = require("x-prod-yippee");
+
+const USERNAME = "Ruben.Wiza24",
+	PASSWORD = readline.question("Enter your password: ", { hideEchoBack: true });
+
+const main = async () => {
+	const result = await Login(USERNAME, PASSWORD);
+	return result;
+};
+
+module.exports = main;
